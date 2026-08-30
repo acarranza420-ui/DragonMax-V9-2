a01ce47b87fe90b743a2cb5f80381fe7c0178034
acarranza420-ui <acarranza420@gmail.com>
2026-08-30 01:48:17 -0700

    Bundle Skin Shortcuts to remove Fire TV dependency timing

--- repo_release.py
+++ repo_release.py
@@ -21,6 +21,7 @@ _FLIGHT_PACK = {
     'resource.images.moviegenreicons.transparent': 'https://mirrors.kodi.tv/addons/matrix/resource.images.moviegenreicons.transparent/resource.images.moviegenreicons.transparent-0.0.6.zip',
     'resource.images.studios.coloured': 'https://mirrors.kodi.tv/addons/omega/resource.images.studios.coloured/resource.images.studios.coloured-0.0.24.zip',
     'resource.images.studios.white': 'https://mirrors.kodi.tv/addons/omega/resource.images.studios.white/resource.images.studios.white-0.0.34.zip',
+    'script.skinshortcuts': 'https://github.com/MikeSiLVO/script.skinshortcuts/releases/download/v2.0.3/script.skinshortcuts-2.0.3.zip',
 }
 
 _builder_state = {'packages': False, 'pruner': False}
